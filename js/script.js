c46b10df590fa268057bed634f7fbf258b39becb
Paweł Matysiak <mat.pawel@gmail.com>
2020-04-05 11:51:31 +0200

    Add computer move

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
-}
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to ' +randomNumber);
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+let computerMove = 'nieznany ruch';
 
-let computerMove = 'kamień';
-let playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + 'to wygrywasz!');
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber ==2){
+	computerMove = 'papier';
+} else if (randomNumber ==3){
+	computerMove = 'nożyce';
+}
+printMessage('Mój ruch to: ' + computerMove);
